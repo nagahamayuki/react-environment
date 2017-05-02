43de7a4cb76f346750567ccc357485e48601846c
nagahamayuki <nagahamayuki@newrevo.jp>
2017-05-02 12:47:41 +0900

    landing page ver.1.1

--- src/app.js
+++ src/app.js
@@ -1,15 +1,33 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react'
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+let store = createStore(todoApp)
+
+const todoApp = (state = {}, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return{
+        id: action.id
+      }
+    default:
+
+  }
+}
 
 class App extends React.Component{
   render(){
     return(
-      <div>helllosssssssssssssss</div>
+      <div>hello</div>
     );
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("app")
-);
+)
